c929519e2efc962edd0c2ef4ec48ac00425b5b44
Alan Bacha <alanbacha@gmail.com>
2020-04-11 18:05:59 -0400

    Allowing cross domain

--- src/app.js
+++ src/app.js
@@ -2,13 +2,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Requiring the configuration of cross domain in the file bellow
+const crossdomain = require("./utils/crossdomain");
+
 // Requiring the routes whose was cofigured in the file bellow
 const postRoutes = require("./routes");
 
 dotenv.config();
 
-// middleware
+// Middleware
 const app = express();
+
+// Using the configuration of cross domain
+app.use(crossdomain);
 
 // Adding json App
 app.use(express.json());
